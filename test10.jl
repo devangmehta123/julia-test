ca6539d9a10946b032be6f679543a6e12e5cde73
Devang Mehta <Devang.Mehta@energylinx.com>
2019-08-05 11:05:34 +0100

    structs again

---
+++ test10.jl
@@ -0,0 +1,18 @@
+"""
+Represents the time of day.
+
+fields: hour, minute, second
+"""
+struct MyTime
+    hour
+    minute
+    second
+end
+time = MyTime(11, 59, 30)
+time2 = MyTime(1, 0, 0)
+
+function addtime(t1, t2)
+    MyTime(t1.hour + t2.hour, t1.minute + t2.minute, t1.second + t2.second)
+end
+
+@show addtime(time, time2)
