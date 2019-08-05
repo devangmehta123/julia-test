406e2644ea9ad6bd49b7863a39568e6c4377e8e1
Devang Mehta <Devang.Mehta@energylinx.com>
2019-08-05 06:02:42 +0100

    test 7

---
+++ test7.jl
@@ -0,0 +1,11 @@
+known = Dict(0=>0, 1=>1)
+function fibonacci(n)
+    if n ∈ keys(known)
+        return known[n]
+    end
+    res = fibonacci(n-1) + fibonacci(n-2)
+    known[n] = res
+    res
+end
+println(fibonacci(20))
+println(fibonacci(30))
